5f4b983e129bc196e4d170b47ae01055c4f1ae28
Algus <algukenan@gmail.com>
2024-12-24 20:17:45 +0300

    Continuing RPG Project
    -Attack to enemy methods.

--- RPG Project/Assets/Scripts/Controller/PlayerController.cs
+++ RPG Project/Assets/Scripts/Controller/PlayerController.cs
@@ -1,3 +1,4 @@
+using RPG.Combat;
 using RPG.Movement;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,6 +11,12 @@ namespace RPG.Controller
 
         void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
@@ -17,16 +24,41 @@ namespace RPG.Controller
             }
         }
 
+        void InteractWithCombat()
+        {
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach(RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                
+                if(target == null)
+                {
+                    continue;
+                }
+                if(Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Ray ray = GetMouseRay();
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
                 //MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 
